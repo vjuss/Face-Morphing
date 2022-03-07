5d5f7cdbdf258dd5e41e7bdc570b941fc53ec71b
Veera <veera.i.jussila@gmail.com>
2022-03-07 17:51:47 +0000

    draw faces from both feeds on one video capture

--- addWeighted_detect.py
+++ addWeighted_detect.py
@@ -1,7 +1,7 @@
 #trying this:
 #two video feeds on top of each other (blended)
-#making opencv find landmarks in the blended frame (also try separately before they are blended), basic detcttion is from https://www.youtube.com/watch?v=MrRGVOhARYY
-#draw these landmarks
+#making opencv find landmarks in the blended frame basic detcttion is from https://www.youtube.com/watch?v=MrRGVOhARYY
+#also detect separately from two inputs, shwo only one feed but draw both faces 
 #print "one face" if faces "match", ie only one face found in the synce, print "two faces" when two are found
 #play with addweighted value: this affewcst results, there could be two output results with different added weights and different efects 
 #think about the effects: will the image be blurred, delanay, filter etc when match and what happens when that connection breaks? 
@@ -16,17 +16,22 @@ import dlib
 from PIL import Image
 
 cap = cv2.VideoCapture(1)
+cap2 = cv2.VideoCapture(0)
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("data/68_face_landmarks.dat")
 
 while True:
 
     ret1, frame = cap.read()
-    if ret1==False:
+    ret2, frame2= cap2.read()
+
+    if ret1==False or ret2==False:
         break
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
     faces= detector(gray)
+    faces2= detector(gray2)
     
     for face in faces:
         x1 = face.left()
@@ -41,7 +46,21 @@ while True:
                 y = landmarks.part(n).y
                 cv2.circle(frame, (x, y), 6, (0, 255, 0), -1)
 
-    cv2.imshow("Frame", frame)
+
+    for face2 in faces2:
+        x1 = face2.left()
+        y1 = face2.top()
+        x2 = face2.right()
+        y2 = face2.bottom()
+        #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+        landmarks2 = predictor(gray2,face2)
+
+        for n in range(0, 68):
+                x = landmarks2.part(n).x
+                y = landmarks2.part(n).y
+                cv2.circle(frame, (x, y), 6, (255, 0, 0), -1)
+
+    cv2.imshow("Frame", frame) #show the other video feed only
 
     key = cv2.waitKey(1)
     if key == 27: #esc
