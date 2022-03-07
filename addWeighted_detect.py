93a306c756a9d8f0fd83050e9911ef8677a8e59a
Veera <veera.i.jussila@gmail.com>
2022-03-07 17:27:03 +0000

    first commit

---
+++ addWeighted_detect.py
@@ -0,0 +1,51 @@
+#trying this:
+#two video feeds on top of each other (blended)
+#making opencv find landmarks in the blended frame (also try separately before they are blended), basic detcttion is from https://www.youtube.com/watch?v=MrRGVOhARYY
+#draw these landmarks
+#print "one face" if faces "match", ie only one face found in the synce, print "two faces" when two are found
+#play with addweighted value: this affewcst results, there could be two output results with different added weights and different efects 
+#think about the effects: will the image be blurred, delanay, filter etc when match and what happens when that connection breaks? 
+# will the resulting effect break the connection by blurring etc. making face not visibles
+#study this for effects https://docs.opencv.org/4.x/d2/d96/tutorial_py_table_of_contents_imgproc.html
+#will the effect bridge time gaps instead of two people? or both
+
+
+import cv2
+import numpy as np
+import dlib
+from PIL import Image
+
+cap = cv2.VideoCapture(1)
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("data/68_face_landmarks.dat")
+
+while True:
+
+    ret1, frame = cap.read()
+    if ret1==False:
+        break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces= detector(gray)
+    
+    for face in faces:
+        x1 = face.left()
+        y1 = face.top()
+        x2 = face.right()
+        y2 = face.bottom()
+        #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+        landmarks = predictor(gray,face)
+
+        for n in range(0, 68):
+                x = landmarks.part(n).x
+                y = landmarks.part(n).y
+                cv2.circle(frame, (x, y), 6, (0, 255, 0), -1)
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27: #esc
+        break
+
+
+# cv2.destroyAllWindows() # destroys the window showing image
